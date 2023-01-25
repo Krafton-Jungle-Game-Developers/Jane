d829ea7b3d22c29556464c34669353da57379a26
LSH <gadlsh1208@naver.com>
2023-01-25 23:04:35 +0900

    Add Unity, Server Project

---
+++ Jane.Server/Jane.Server/Program.cs
@@ -0,0 +1,12 @@
+using MagicOnion;
+using MagicOnion.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+builder.Services.AddMagicOnion();
+
+var app = builder.Build();
+app.MapMagicOnionService();
+
+app.Run();
