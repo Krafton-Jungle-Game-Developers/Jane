94da64c41d949723c4342fb2242fe5487b009901
SeungHu Lee <gadlsh12081208@gmail.com>
2023-02-21 04:12:33 +0900

    Working on Movement Sync

--- Jane.Unity/Assets/Scripts/ServerShared/Hubs/IMovementHub.cs
+++ Jane.Unity/Assets/Scripts/ServerShared/Hubs/IMovementHub.cs
@@ -8,8 +8,8 @@ namespace Jane.Unity.ServerShared.Hubs
 {
     public interface IMovementHub : IStreamingHub<IMovementHub, IMovementHubReceiver>
     {
-        ValueTask<MoveRequest[]> JoinAsync(Ulid roomId, Ulid userId, Vector3 position, Quaternion rotation);
+        ValueTask<Player[]> JoinAsync(Ulid roomId, Ulid userId, Vector3 position, Quaternion rotation);
         ValueTask LeaveAsync();
-        ValueTask MoveAsync(Vector3 position, Quaternion rotation);
+        ValueTask MoveAsync(MoveRequest request);
     }
 }
