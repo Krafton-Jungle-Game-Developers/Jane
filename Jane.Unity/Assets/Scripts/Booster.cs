cb395fd148090f0be08fd1c2462ffb30a109c2cf
LemonEaterMJ <98jjyoung@gmail.com>
2023-02-21 23:48:47 +0900

    booster effect _inprogress

--- Jane.Unity/Assets/Scripts/Booster.cs
+++ Jane.Unity/Assets/Scripts/Booster.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Booster : MonoBehaviour
 {
     SpaceshipController spaceshipController;
-    [SerializeField] VisualEffect boosterImpactVFX;
-    [SerializeField] KeyCode boosterKey = KeyCode.LeftShift;
-    [SerializeField] float boosterSpeed = 400f;
-    [SerializeField] float normalSpeed = 200f;
+    [SerializeField] private VisualEffect boosterImpactVFX;
+    [SerializeField] private VisualEffect boosterLoopVFX;
+    [SerializeField] private KeyCode boosterKey = KeyCode.LeftShift;
+    [SerializeField] private float boosterSpeed = 400f;
+    [SerializeField] private float normalSpeed = 200f;
+    [SerializeField] private float _warpRate = 0.02f;
     private bool _boosterActive;
 
     private void Awake()
@@ -21,6 +23,8 @@ public class Booster : MonoBehaviour
     void Start()
     {
         boosterImpactVFX.Stop();
+        boosterLoopVFX.Stop();
+        boosterLoopVFX.SetFloat("WarpAmount", 0);
     }
 
     
@@ -46,11 +50,29 @@ public class Booster : MonoBehaviour
         {
             yield return new WaitForSeconds(0.6f);
             spaceshipController.ChangeSpeed(boosterSpeed);
+            boosterLoopVFX.Play();
+
+            float _warpAmount = boosterLoopVFX.GetFloat("WarpAmount");
+            while (_warpAmount < 1) 
+            {
+                _warpAmount += _warpRate;
+                boosterLoopVFX.SetFloat("WarpAmount", _warpAmount);
+            }
         }
         else
         {
             yield return new WaitForSeconds(0.1f);
             spaceshipController.ChangeSpeed(normalSpeed);
+
+            float _warpAmount = boosterLoopVFX.GetFloat("WarpAmount");
+            while (_warpAmount >= _warpRate)
+            {
+                _warpAmount -= _warpRate;
+                boosterLoopVFX.SetFloat("WarpAmount", _warpAmount);
+            }
+            //boosterLoopVFX.SetFloat("WarpAmount", 0);
+
+            boosterLoopVFX.Stop();
         }
     }
 }
