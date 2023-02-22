fe6b23361de48c5cd09a0bdc637c008dcbdb6a33
snape01265 <swl6507@gmail.com>
2023-02-22 15:53:07 +0900

    auto pivot to x plane

--- Jane.Unity/Assets/Game/Scripts/SpaceshipController.cs
+++ Jane.Unity/Assets/Game/Scripts/SpaceshipController.cs
@@ -13,7 +13,9 @@ public class SpaceshipController : MonoBehaviour
     [SerializeField] private float forwardSpeed = 200f, strafeSpeed = 50f, hoverSpeed = 50f, rollSpeed = 5f;
     private float _activeForwardSpeed, _activeStrafeSpeed, _activeHoverSpeed;
     [SerializeField] private float _forwardAcceleration = 2f, _strafeAcceleration = 2f, _hoverAcceleration = 2f, rollAcceleration = 0.5f;
-    private float _rollInput;
+    private float _activeRollSpeed;
+    [SerializeField] private float _rollBackSpeed = 5f;
+    [SerializeField] private AnimationCurve curve;
     private Vector3 _lookInput, _screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f), _mouseDistance;
 
     public RectTransform cursorRectTransform;
@@ -28,13 +30,12 @@ public class SpaceshipController : MonoBehaviour
     {
         MouseSteeringUpdate();
         MovementUpdate();
-        Debug.Log("Active Forward Speed : " + _activeForwardSpeed);
+        RollUpdate();
     }
 
     private void MouseSteeringUpdate()
     {
-/*        if (_lookInput == )
-*/        // Consistent method
+        // Consistent method
         _lookInput = cursorRectTransform.position;
 
         _mouseDistance.x = (_lookInput.x - _screenCenter.x) / _screenCenter.y;
@@ -42,27 +43,26 @@ public class SpaceshipController : MonoBehaviour
 
         _mouseDistance = Vector3.ClampMagnitude(_mouseDistance, 1f);
 
-        _rollInput = Mathf.Lerp(_rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
-
-        transform.Rotate(-_mouseDistance.y * lookRateSpeed, _mouseDistance.x * lookRateSpeed, _rollInput * rollSpeed, Space.Self);
-
-        // Raw input method
-/*        else if (steerVersion == 2)
+        if (_mouseDistance.magnitude < 0.1f)
         {
-            float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
-            float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+            return;
+        }
 
-            _yRotation -= mouseX;
-            _xRotation -= mouseY;
-
-            _mouseDistance = new Vector3(mouseX, mouseX, 0f);
-
-            _rollInput = Mathf.Lerp(_rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
-
-            transform.rotation = Quaternion.Euler(0, _yRotation, 0);
-            transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
-        }*/
+        transform.Rotate(-_mouseDistance.y * lookRateSpeed, _mouseDistance.x * lookRateSpeed, 0f, Space.Self);
     }
+
+    float t;
+    private void RollUpdate()
+    {
+        float rollInput = Input.GetAxisRaw("Roll");
+        _activeRollSpeed = Mathf.Lerp(_activeRollSpeed, rollInput, rollAcceleration * Time.deltaTime);
+        if(Mathf.Abs(rollInput) < 0.1f)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f), Mathf.Exp(_rollBackSpeed * 0.1f) * Time.deltaTime);
+        }
+        transform.Rotate(0f, 0f, _activeRollSpeed * rollSpeed, Space.Self);
+    }
+
     private void MovementUpdate()
     {
         _activeForwardSpeed = Mathf.Lerp(_activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, Time.deltaTime * _forwardAcceleration);
@@ -73,7 +73,6 @@ public class SpaceshipController : MonoBehaviour
                               (transform.right * _activeStrafeSpeed * Time.deltaTime) +
                               (transform.up * _activeHoverSpeed * Time.deltaTime);
     }
-
 
     // Method for Controlling Speed by outside elements 
     // Currently used by BOOSTER.cs 
