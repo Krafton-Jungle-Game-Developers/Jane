365f97e62af0b17acb7d01d911f06386245ff2ff
LemonEaterMJ <98jjyoung@gmail.com>
2023-02-23 02:41:42 +0900

    portal vfx FIN

--- Jane.Unity/Assets/Scripts/Gate.cs
+++ Jane.Unity/Assets/Scripts/Gate.cs
@@ -9,15 +9,18 @@ using UnityEngine.VFX;
 public class Gate : MonoBehaviour
 {
     [SerializeField] private VisualEffect gateActivateVFX;
+    [SerializeField] private ParticleSystem swirlGreen;
 
     public void Activate()
     {
         // Probably some Activation VFX 
         gateActivateVFX.Play();
+        swirlGreen.Play();
     }
 
     public void Deactivate()
     {
         gateActivateVFX.Stop();
+        swirlGreen.Stop();
     }
 }
