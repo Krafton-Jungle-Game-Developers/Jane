7a33ba83a6828ce8714f4e9ef9d872f44a7ac03a
LemonEaterMJ <98jjyoung@gmail.com>
2023-02-27 00:38:08 +0900

    gate SFX

--- Jane.Unity/Assets/Scripts/Gate.cs
+++ Jane.Unity/Assets/Scripts/Gate.cs
@@ -10,16 +10,25 @@ public class Gate : MonoBehaviour
 {
     [SerializeField] private VisualEffect gateActivateVFX;
     [SerializeField] private ParticleSystem swirlGreen;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip activateSFX;
+    [SerializeField] private AudioClip gateClearSFX;    // Used for user Feedback
 
     public void Activate()
     {
+        audioSource.clip = activateSFX;
+
         // Probably some Activation VFX 
         gateActivateVFX.Play();
+        audioSource.Play();
         swirlGreen.Play();
     }
 
     public void Deactivate()
     {
+        audioSource.clip = gateClearSFX;
+        
+        audioSource.Play();
         gateActivateVFX.Stop();
         swirlGreen.Stop();
     }
