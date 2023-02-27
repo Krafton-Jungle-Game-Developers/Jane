4f728df1422bfd924b17619104e40945c3692537
snape01265 <swl6507@gmail.com>
2023-02-27 21:57:54 +0900

    HUD attempt

---
+++ Jane.Unity/Assets/Game/Scripts/WarpGate.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class WarpGate : MonoBehaviour
+{
+    [SerializeField] private float maxFOV;
+    [SerializeField] private float maxBloomIntensity;
+    [SerializeField] private float duration;
+    [SerializeField] private PlayerCameraEffect playerCameraEffect;
+    public GameObject warpDestination;
+    private GameObject _player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _player = other.gameObject;
+            StartCoroutine(Warp());
+        }
+    }
+
+    IEnumerator Warp()
+    {
+        float timeElapsed = 0f;
+        float startingFOV = playerCameraEffect.nowFOV;
+        float startingBloom = playerCameraEffect.nowBloomIntensity;
+
+        //some method in controller that stops player input & movement
+        _player.GetComponent<SpaceshipController>().canControl = false;
+
+        while(timeElapsed < duration)
+        {
+            float newFOV = Mathf.Lerp(startingFOV, maxFOV, Mathf.Pow(timeElapsed / duration, 3f));
+            float newBloomIntensity = Mathf.Lerp(startingBloom, maxBloomIntensity, Mathf.Pow(timeElapsed / duration, 2f));
+            playerCameraEffect.nowFOV = newFOV;
+            playerCameraEffect.nowBloomIntensity = newBloomIntensity;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        _player.transform.position = warpDestination.transform.position;
+        timeElapsed = 0f;
+        playerCameraEffect.nowFOV = maxFOV;
+        playerCameraEffect.nowBloomIntensity = maxBloomIntensity;
+        while(timeElapsed < duration)
+        {
+            float newFOV = Mathf.Lerp(maxFOV, playerCameraEffect.baseFOV, Mathf.Pow(timeElapsed / duration, 4f));
+            float newBloomIntensity = Mathf.Lerp(maxBloomIntensity, playerCameraEffect.baseBloomIntensity, Mathf.Pow(timeElapsed / duration, 2f));
+            playerCameraEffect.nowFOV = newFOV;
+            playerCameraEffect.nowBloomIntensity = newBloomIntensity;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        _player.GetComponent<SpaceshipController>().canControl = true;
+    }
+}
