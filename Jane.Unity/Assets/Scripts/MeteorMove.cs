cbea20ec5719e69c24b45f158db040ea76004c98
LemonEaterMJ <98jjyoung@gmail.com>
2023-03-04 17:34:59 +0900

    meteor spawner prototype fin TODO : polishing

--- Jane.Unity/Assets/Scripts/MeteorMove.cs
+++ Jane.Unity/Assets/Scripts/MeteorMove.cs
@@ -8,60 +8,77 @@ using UnityEngine.UIElements;
 /// Start and End should be controlled by outside Scripts. 
 /// Speed is also controlled by outside components.
 /// </summary>
-public class MeteorMove : MonoBehaviour
-{
-    private GameObject SpawnController;
-    private SpawnMeteor _spawnMeteor;
+//public class MeteorMove : MonoBehaviour
+//{
+//    private GameObject SpawnController;
+//    private SpawnMeteor _spawnMeteor;
 
-    public float _speed;
-    [SerializeField] GameObject _impactPrefab;
+//    public float _speed;
+//    [SerializeField] GameObject _impactPrefab;
 
-    private Rigidbody _rb;
+//    private Rigidbody _rb;
 
-    void Start()
-    {
-        SpawnController = GameObject.FindWithTag("Spawner");
-        _spawnMeteor = SpawnController.GetComponent<SpawnMeteor>();
-        _rb = GetComponent<Rigidbody>();    
-    }
+//    void Start()
+//    {
+//        SpawnController = GameObject.FindWithTag("Spawner");
+//        _spawnMeteor = SpawnController.GetComponent<SpawnMeteor>();
+//        _rb = GetComponent<Rigidbody>();    
+//    }
     
-    void Update()
-    { 
-        Destroy(gameObject, _spawnMeteor.destroyTime);
-    }
+//    void Update()
+//    { 
+//        Destroy(gameObject, _spawnMeteor.destroyTime);
+//    }
 
-    private void FixedUpdate()
-    {   
-        if (_speed != 0 && _rb != null)
-        {
-            _rb.position += transform.forward * (_speed * Time.deltaTime);
-        } 
+//    private void FixedUpdate()
+//    {   
+//        if (_speed != 0 && _rb != null)
+//        {
+//            _rb.position += transform.forward * (_speed * Time.deltaTime);
+//        } 
 
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        // _speed = 0;
-
-        if (other.gameObject.CompareTag("MeteorPoint"))
-        {
-            // Reach Checkpoint
-            
-        } 
-        else
-        {
-            ContactPoint contact = other.contacts[0];
-            Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
-            Vector3 position = contact.point;
-
-            if (_impactPrefab != null)
-            {
-                var impactVFX = Instantiate(_impactPrefab, position, rotation) as GameObject;
-                Destroy(impactVFX, 3);
-            }
-
-        }
+//    }
 
 
-    }
-}
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        Debug.Log($"OnTriggerEnter Called. other's tag was {other.tag}");
+//        if (other.gameObject.CompareTag("MeteorPoint"))
+//        {
+//            Debug.Log("Triggered");
+//            // Reach Checkpoint
+//            int midIdx = System.Array.IndexOf(_spawnMeteor.middlePoints, other);
+//            if (midIdx >= _spawnMeteor.middlePoints.Length - 1)
+//            {
+//                // Last CheckPoint
+//                Destroy(gameObject);
+//            }
+//            else
+//            {
+//                _spawnMeteor.RotateTo(gameObject, _spawnMeteor.middlePoints[midIdx + 1].transform.position);
+//            }
+
+
+//        }
+//    }
+
+//    private void OnCollisionEnter(Collision other)
+//    {
+//        // _speed = 0;
+
+
+//        ContactPoint contact = other.contacts[0];
+//        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+//        Vector3 position = contact.point;
+
+//        if (_impactPrefab != null)
+//        {
+//            var impactVFX = Instantiate(_impactPrefab, position, rotation) as GameObject;
+//            Destroy(impactVFX, 2);
+//        }
+
+        
+
+
+//    }
+//}
